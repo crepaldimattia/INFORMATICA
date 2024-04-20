f7e4d6900f776b5397e0cbd887a92188feabeb5d
crepaldimattia <150442472+crepaldimattia@users.noreply.github.com>
2024-04-20 17:24:47 +0200

    Add files via upload

---
+++ ESERCIZIO RASPA/src/tools/Contatto.java
@@ -0,0 +1,12 @@
+package tools;
+public class Contatto {
+    public String nome;
+    public String cognome;
+    public String telefono;
+    public double ricarica;
+
+    /*public Contatto(String nome, String cognome ){  //Ci dovrebbero essere anche le altre variabili
+        this.nome=nome;  //this identifica l'attributo dell'oggetto --> mettere il nome a private
+        this.ricarica=10;
+    }*/
+}
